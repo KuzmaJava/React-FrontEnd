0fd9984025b2bc7cc2a4b37037c8e0c5cd9fc846
Yevhenii Kuzmenko <zheniakuzma@gmail.com>
2023-05-08 14:23:14 +0300

    Created Add Employee Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
+import CreateEmployeeComponent from './components/CreateEmployeeComponent'
 import HeaderComponent from './components/HeaderComponent'
 import FooterComponent from './components/FooterComponent'
 
@@ -13,8 +14,9 @@ function App() {
                 <HeaderComponent />
                   <div className="container">
                       <Routes>
-                             <Route path = "/" element = {<ListEmployeeComponent/>}/>
+                             <Route path = "/" exact element = {<ListEmployeeComponent/>}/>
                              <Route path = "/employees" element = {<ListEmployeeComponent/>}/>
+                             <Route path = "/add-employee" element = {<CreateEmployeeComponent/>}/>
                       </Routes>
                   </div>
                 <FooterComponent />
